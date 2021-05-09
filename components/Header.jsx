71417c8528d145d11c4413504216fad7be2b1ba2
Vikrant Bhat <vik.bhat223@gmail.com>
2021-05-10 03:42:10 +0530

    <Header /> done

--- components/Header.jsx
+++ components/Header.jsx
@@ -15,9 +15,14 @@ import {
 
 const Header = () => {
 	return (
-		<header className="">
-			<div className="">
-				<HeaderItem title="Home" Icon={HomeIcon} />
+		<header className="flex flex-col sm:flex-row m-5 justify-between items-center">
+			<div className="flex flex-grow justify-evenly max-w-2xl h-auto">
+				<HeaderItem title="HOME" Icon={HomeIcon} />
+				<HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+				<HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+				<HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+				<HeaderItem title="SEARCH" Icon={SearchIcon} />
+				<HeaderItem title="ACCOUNT" Icon={UserIcon} />
 			</div>
 			<Image
 				src="https://links.papareact.com/ua6"
