686e411bbef2565d2529458594a438ecbadacf81
Vikrant Bhat <vik.bhat223@gmail.com>
2021-05-15 14:26:37 +0530

    styling Thumbnail

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -12,7 +12,7 @@ const Thumbnail: React.FC<Props> = ({ result }) => {
 	const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original/";
 
 	return (
-		<div>
+		<div className='group cursor-pointer'>
 			<Image
 				src={
 					`${IMAGE_BASE_URL}${result.backdrop_path || result.poster_path}` ||
@@ -24,10 +24,13 @@ const Thumbnail: React.FC<Props> = ({ result }) => {
 			/>
 			<div className="p-2">
 				<p className="truncate max-w-md">{result.overview}</p>
-				<h2>{result.title || result.original_name}</h2>
-				<p>
-					{/* {result.media_type &&} */}
-					<ThumbUpIcon />
+
+				<h2 className='mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold'>{result.title || result.original_name}</h2>
+
+				<p className='flex items-center opacity-0 group-hover:opacity-100'>
+					{result.media_type && `${result.media_type} • `}
+					{result.release_date || result.first_air_date} • {' '}
+					<ThumbUpIcon className='h-5 mx-2' /> {result.vote_count}
 				</p>
 			</div>
 		</div>
